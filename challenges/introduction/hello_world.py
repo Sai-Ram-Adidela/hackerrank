c4638786f5dc32cc380d9cf222c0b175abcd959b
Sai Rama Krishna Reddy <adidalasairam@gmail.com>
2018-04-20 15:32:14 +0530

    Create hello_world.py

---
+++ challenges/introduction/hello_world.py
@@ -0,0 +1,9 @@
+"""
+Title     : Hello World
+Subdomain : Challenges/Introduction
+Domain    : Python
+Author    : Sai Ram Adidela
+Created   : 18 April 2018
+"""
+if __name__ == '__main__':
+    print("Hello, World!")
