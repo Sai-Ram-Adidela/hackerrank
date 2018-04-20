cbfde4c33ccdff9212f3c1e93a2377a3c92151f2
Sai Rama Krishna Reddy <adidalasairam@gmail.com>
2018-04-20 15:34:18 +0530

    Create if_else.py

---
+++ challenges/basic data types/if_else.py
@@ -0,0 +1,18 @@
+"""
+Title     : if_else
+Subdomain : Challenges/Basic Data Types
+Domain    : Python
+Author    : Sai Ram Adidela
+Created   : 18 April 2018
+"""
+if __name__ == '__main__':
+    n = int(input())
+    if n % 2 == 0:
+        if 2 <= n <= 5:
+            print("Not Weird")
+        elif 6 <= n <= 20:
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
+    else:
+        print("Weird")
