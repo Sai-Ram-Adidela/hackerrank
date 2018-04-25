5c7540d5720587d62040a71df8e3f22792d1db2d
Sai Rama Krishna Reddy <adidalasairam@gmail.com>
2018-04-25 14:19:33 +0530

    Update euler003.py

--- Project Euler/euler003.py
+++ Project Euler/euler003.py
@@ -1,32 +1,33 @@
-'''
+"""
 Title     : euler003/Largest Prime Factor(euler003)
 Subdomain : ProjectEuler+
 Domain    : Python
 Author    : Sai Ram Adidela
 Created   : 19 April 2018
-'''
+"""
 
 import math
 
 
-def maxPrimeFactors(n):
-	maxPrime = -1
+def max_prime_factors(n):
+    maxprime = -1
 
-	while n % 2 == 0:
-		maxPrime = 2
-		n >>= 1	 # equivalent to n /= 2
+    while n % 2 == 0:
+        maxprime = 2
+        n >>= 1  # equivalent to n /= 2
 
-	for i in range(3, int(math.sqrt(n)) + 1, 2):
-		while n % i == 0:
-			maxPrime = i
-			n = n / i
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            maxprime = i
+            n = n / i
 
-	if n > 2:
-		maxPrime = n
-	
-	return int(maxPrime)
+    if n > 2:
+        maxprime = n
+
+    return int(maxprime)
+
 
 t = int(input())
-for i in range(t):
-	n = int(input())
-	print(maxPrimeFactors(n))
+for _ in range(t):
+    k = int(input())
+    print(max_prime_factors(k))
