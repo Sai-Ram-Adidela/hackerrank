feb71be44f194a9e729acb0a2e71eeae8ba1a608
Sai Rama Krishna Reddy <adidalasairam@gmail.com>
2018-04-25 14:06:47 +0530

    Update day3_conditional_statements.py

--- 30_days_Of_Code/day3_conditional_statements.py
+++ 30_days_Of_Code/day3_conditional_statements.py
@@ -1,24 +1,22 @@
-'''
-Title     : conditional_statements
+"""
+Title     : day3_conditional_statements
 Subdomain : 30 days of code
 Domain    : Python
 Author    : Sai Ram Adidela
 Created   : 18 April 2018
-'''
+"""
 N = input().strip()
 
 if not N.isdigit():
     print("Not Weird")
 else:
     N = int(N)
-    if N%2 ==0:
-        if N<=5 and N>=2:
+    if N % 2 == 0:
+        if 5 >= N >= 2:
             print("Not Weird")
-        elif N<=20 and N>=6:
+        elif 20 >= N >= 6:
             print("Weird")
-        elif N>20:
+        elif N > 20:
             print("Not Weird")
     else:
         print("Weird")
-    
-
