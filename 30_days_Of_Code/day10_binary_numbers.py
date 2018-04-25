adeaea106dbf07cd0fcb3545bac24a705045278c
Sai Rama Krishna Reddy <adidalasairam@gmail.com>
2018-04-25 14:10:06 +0530

    Update day10_binary_numbers.py

--- 30_days_Of_Code/day10_binary_numbers.py
+++ 30_days_Of_Code/day10_binary_numbers.py
@@ -1,23 +1,23 @@
-'''
-Title     : Binary Numbers
+"""
+Title     : day10_Binary Numbers
 Subdomain : 30 days of code
 Domain    : Python
 Author    : Sai Ram Adidela
 Created   : 18 April 2018
-'''
+"""
 n = int(input())
 
 max_one_count = 0
 one_count = 0
 
 while n != 0:
-	factor = n // 2
-	remainder = n - 2 * factor
-	n = factor
-	if remainder == 1:
-		one_count += 1
-		max_one_count = max(max_one_count, one_count)
-	else:
-		one_count = 0
+    factor = n // 2
+    remainder = n - 2 * factor
+    n = factor
+    if remainder == 1:
+        one_count += 1
+        max_one_count = max(max_one_count, one_count)
+    else:
+        one_count = 0
 
 print(max_one_count)
