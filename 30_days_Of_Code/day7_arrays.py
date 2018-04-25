67e5d76084ecedca611bb9d44ce4199b3c050b59
Sai Rama Krishna Reddy <adidalasairam@gmail.com>
2018-04-25 14:08:52 +0530

    Update day7_arrays.py

--- 30_days_Of_Code/day7_arrays.py
+++ 30_days_Of_Code/day7_arrays.py
@@ -1,10 +1,10 @@
-'''
-Title     : Arrays
+"""
+Title     : day7_Arrays
 Subdomain : 30 days of code
 Domain    : Python
 Author    : Sai Ram Adidela
 Created   : 18 April 2018
-'''
+"""
 n = int(input())
 arr = [i for i in input().strip().split(' ')]
 # print(arr)
@@ -14,4 +14,3 @@ arr = [i for i in input().strip().split(' ')]
 # print(srev)
 rarr = map(str, arr[::-1])
 print(' '.join(rarr))
-
