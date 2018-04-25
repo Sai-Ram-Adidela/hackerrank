f1e1525cd2f57a4f8c8d3d201b3e0880e4b1dd84
Sai Rama Krishna Reddy <adidalasairam@gmail.com>
2018-04-25 14:08:04 +0530

    Update day5_loops.py

--- 30_days_Of_Code/day5_loops.py
+++ 30_days_Of_Code/day5_loops.py
@@ -1,10 +1,10 @@
-'''
-Title     : loops
+"""
+Title     : day5_loops
 Subdomain : 30 days of code
 Domain    : Python
 Author    : Sai Ram Adidela
 Created   : 18 April 2018
-'''
+"""
 n = int(input().strip())
-for i in range(1,11):
-    print(n ,'x', i ,'=', n*i)
+for i in range(1, 11):
+    print(n, 'x', i, '=', n*i)
