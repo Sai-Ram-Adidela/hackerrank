a66c6b9cc55e3182ede0cf5e5c79ca0d5b8103f0
Sai Rama Krishna Reddy <adidalasairam@gmail.com>
2018-04-25 14:18:44 +0530

    Update euler001.py

--- Project Euler/euler001.py
+++ Project Euler/euler001.py
@@ -1,20 +1,22 @@
-'''
+"""
 Title     : euler001/Multiples of 3 and 5
 Subdomain : ProjectEuler+
 Domain    : Python
 Author    : Sai Ram Adidela
 Created   : 19 April 2018
-'''
+"""
 
 
-def sum(n, k):
+def su(n, k):
     d = n // k
     return k * (d * (d+1)) // 2
-  
+
+
 def euler1(n):
-    return sum(n, 3) + sum(n, 5) - sum(n, 15)
+    return su(n, 3) + su(n, 5) - su(n, 15)
+
 
 t = int(input().strip())
 for i in range(t):
     N = int(input().strip())
-    print(euler1(N - 1)) # below N
+    print(euler1(N - 1))  # below N
