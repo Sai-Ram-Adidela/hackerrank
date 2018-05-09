8f340c3972761cbd756e2b66b84e50c571f683fe
Sai Rama Krishna Reddy <adidalasairam@gmail.com>
2018-05-09 10:54:21 +0530

    Update finding_the_percentage.py

--- challenges/basic data types/finding_the_percentage.py
+++ challenges/basic data types/finding_the_percentage.py
@@ -1,6 +1,6 @@
 """
 Title     : Finding the Percentage
-Subdomain : Challenges/Basic Data Types
+Subdomain : HackerRank/Python3/Challenges/Basic Data Types
 Domain    : Python
 Author    : Sai Ram Adidela
 Created   : 23 April 2018
